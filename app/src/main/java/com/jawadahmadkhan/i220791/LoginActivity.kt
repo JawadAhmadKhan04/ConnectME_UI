be600f81022f7f7348ee1a322427f8bfb73e36a9
JawadAhmadKhan04 <114512817+JawadAhmadKhan04@users.noreply.github.com>
2025-02-07 23:50:31 +0500

    Login Screen

---
+++ app/src/main/java/com/jawadahmadkhan/i220791/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.jawadahmadkhan.i220791
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        // Find the register text
+//        val registerText = findViewById<TextView>(R.id.registerText)
+
+        // Set click listener on register text
+//        registerText.setOnClickListener {
+//            val intent = Intent(this, RegisterActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
