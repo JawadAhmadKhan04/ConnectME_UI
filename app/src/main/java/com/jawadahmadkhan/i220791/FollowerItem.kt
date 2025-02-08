3b04bbeacc6bb104f9c7ffca52b0ecb1607cd755
JawadAhmadKhan04 <114512817+JawadAhmadKhan04@users.noreply.github.com>
2025-02-08 13:45:30 +0500

    Followers List screen is done
    Need to convert the Recycler View to tabs as presently it is showing only the list

---
+++ app/src/main/java/com/jawadahmadkhan/i220791/FollowerItem.kt
@@ -0,0 +1,2 @@
+package com.jawadahmadkhan.i220791
+data class FollowerItem(val name: String, val imageRes: Int)
---
+++ app/src/main/java/com/jawadahmadkhan/i220791/FollowerListActivity.kt
@@ -0,0 +1,28 @@
+package com.jawadahmadkhan.i220791
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FollowerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.all_followers)
+
+        val FollowerRecyclerView = findViewById<RecyclerView>(R.id.viewPager)
+        FollowerRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        val FollowerList = listOf(
+            FollowerItem("Henry Benjamin", R.drawable.dm_image),
+            FollowerItem("Emily James", R.drawable.dm_image),
+            FollowerItem("Lily Thomas", R.drawable.dm_image),
+            FollowerItem("Christopher", R.drawable.dm_image),
+            FollowerItem("Amy Wesley", R.drawable.dm_image),
+            FollowerItem("Laura Ryan", R.drawable.dm_image)
+        )
+
+        FollowerRecyclerView.adapter = FollowerAdapter(FollowerList)
+    }
+}
