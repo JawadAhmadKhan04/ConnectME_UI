8728b1db4a2a84e61b039fc4df9c11d41a7668b4
JawadAhmadKhan04 <114512817+JawadAhmadKhan04@users.noreply.github.com>
2025-02-10 21:32:58 +0500

    Final new post screen done

---
+++ app/src/main/java/com/jawadahmadkhan/i220791/NewPostActivity.kt
@@ -0,0 +1,14 @@
+package com.jawadahmadkhan.i220791
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_post_final)
+
+    }
+}
